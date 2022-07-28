4a0007d59d23f5151430f52d4ccae9823371f3fa
teguhtsumarno <teguhtsumarno@gmail.com>
2022-07-29 06:44:50 +0700

    menambahkan field dan constructor

--- src/management/NewClass.java
+++ src/management/NewClass.java
@@ -10,9 +10,16 @@ package testing.src.management;
  * @author juanm
  */
 public class NewClass {
+    String name;
+    String coba;
     public static void main(String[] args) {
         System.out.println("COBA TAMBAH DATA");
         System.out.println("juan");
+        
+       public NewClass(String name,String coba){
+           this.name =  name;
+           this.coba = coba;
+           
     }
     
 }
